43e48459f1326d61580b92c254ab3e7283f9cd33
Matias0422 <brunomatias0422@gmail.com>
2019-04-30 20:52:56 -0300

    Add historic screen
    Co-authored-by: CaioNunes <caiofelipe147@gmail.com>

--- src/components/historic/Historic.js
+++ src/components/historic/Historic.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
-import { Container, Header, Content, List, ListItem, Text, Title, FlatList } from 'native-base';
+import { Container, Header, Content, Text, Title, List, FlatList, ListItem } from 'native-base';
 import Show from './Show'
+
+
+const list = [{id: 1, data: '12/01/2019'}, {id: 2, data: '12/01/2019'}]
 
 export default class Historic extends Component {
   render() {
@@ -8,7 +11,16 @@ export default class Historic extends Component {
       <Container>
       <Title style={{ color: 'black', marginTop: '5%', marginBottom: '10%' }}>HISTÓRICO DE FABRICAÇÕES</Title>
         <Content>
-          <FlatList data={[{title: 'Item 1', key: 'item1'}, {title: 'Item 2', key: 'item2'} ]} renderItem={({item}) => <Show/>}/>
+          <List>
+            {
+              list.map( (element) => (
+                <ListItem key={element.id}>
+                  <Text>{element.id}</Text>
+                  <Text>{element.data}</Text>
+                </ListItem>
+              ))
+            }
+          </List>
         </Content>
       </Container>
     );
